b9ca397d2a6de9e093a0dc7d98e353e40a2b7355
seunggu lee <leesg107@tpsg.co.kr>
2021-11-17 23:48:35 +0900

    feat: 404 not foud 페이지 추가

---
+++ src/views/NotFound.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-app>
+        <v-container class="fill-height">
+            <v-row justify="center">
+                <v-col cols="auto">
+                    <v-card>
+                        <v-img :src="require('@/assets/notfound.png')" class="ma-3"></v-img>
+                        <v-card-text class="text-center">죄송합니다. 해당 페이지를 찾지 못 했습니다.</v-card-text>
+                        <div class="text-center pa-5">
+                            <router-link to="/" class="home_link">
+                                <v-btn tile color="success">
+                                    <v-icon left> mdi-home </v-icon>
+                                    홈으로 돌아가기
+                                </v-btn>
+                            </router-link>
+                        </div>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.home_link {
+    text-decoration: none;
+    color: black;
+}
+</style>
