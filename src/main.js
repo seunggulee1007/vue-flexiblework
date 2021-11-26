c10a6f2dbf17784016515ab8edc2406037b05b31
seunggu lee <leesg107@tpsg.co.kr>
2021-11-26 17:50:52 +0900

    feat : vuetify 컬럼 간격 조정 라이브러리 적용

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import VueColumnsResizableVuetify from 'vue-columns-resizable-vuetify';
+
 import VueCookie from 'vue-cookie';
 import VueGeolocationApi from 'vue-geolocation-api';
 
 Vue.use(VueCookie);
 Vue.use(VueGeolocationApi /*, { ...options } */);
+Vue.use(VueColumnsResizableVuetify);
 Vue.config.productionTip = false;
 
 new Vue({
